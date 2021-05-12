415360821bdee13d2da2a3b89096c5ab2907b2f3
Hassene <hassene.ayoub@yahoo.fr>
2021-05-12 22:27:38 +0100

    Gestion des comptes

---
+++ src/Controller/Administrateur/GestionComptesAdminController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller\Administrateur;
+
+use App\Entity\Administrateur;
+use App\Form\AjouterAdministrateurType;
+use App\Repository\AdministrateurRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GestionComptesAdminController extends AbstractController
+{
+
+    /**
+     * @Route("/AffichierAdministrateur{id}", name="affichier_administrateur")
+     */
+    public function affichierAdministrateur(AdministrateurRepository $AdministrateurRepository , $id ){
+
+        return $this->render('/Administrateur/gestion_administrateurs/affichier_administrateur.html.twig', ['administrateur'=> $AdministrateurRepository->find($id)]);
+    }
+    /**
+     * @Route("/ajouterAdministrateur", name="ajouter_administrateur")
+     */
+    public function ajouter(Request $request )
+    {   $Administrateur =new Administrateur();
+        $form = $this->createForm(AjouterAdministrateurType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $Administrateur=$form->getData();
+           $em= $this->getDoctrine()->getManager();
+           $em->persist($Administrateur);
+           $em->flush();
+           return $this->redirectToRoute("admin-dashboard");
+        }
+        return $this->render('/Administrateur/gestion_administrateurs/ajouter_administrateur.html.twig', ['form'=> $form->createView()]);
+    }
+    /**
+     * @Route("/modifierAdministrateur/{id}", name="modifier_administrateur")
+     */
+    public function modifier(Request $request,AdministrateurRepository $AdministrateurRepository,$id )
+    {   $Administrateur=$AdministrateurRepository->find($id);
+        $form = $this->createForm(AjouterAdministrateurType::class,$Administrateur);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $Administrateur=$form->getData();
+           $em= $this->getDoctrine()->getManager();
+           $em->persist($Administrateur);
+           $em->flush();
+           return $this->redirectToRoute("admin-dashboard");
+        }
+        return $this->render('/Administrateur/gestion_administrateurs/modifier_administrateur.html.twig', ['form'=> $form->createView()]);
+    }
+    /**
+     * @Route("/supprimerAdministrateur/{id}", name="supprimer_administrateur")
+     */
+    public function supprimer(AdministrateurRepository $AdministrateurRepository,$id )
+    {   $Administrateur=$AdministrateurRepository->find($id);
+        $em=$this->getDoctrine()->getManager();
+        if(!$Administrateur){
+            throw $this->createNotFoundException("Administrateur n'existe pas");
+        }
+        $em->remove($Administrateur);
+        $em->flush();
+        return $this->redirectToRoute("admin-dashboard");
+    }
+
+   
+}
---
+++ src/Form/AjouterAdministrateurType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Administrateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AjouterAdministrateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Nom')
+            ->add('Prenom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Administrateur::class,
+        ]);
+    }
+}
