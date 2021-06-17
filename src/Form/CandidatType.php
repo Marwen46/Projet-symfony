af77abb666c6b7c3c544722f4fa85ad9c6f5e174
deathsh0ot <ala-hmz@outlook.com>
2021-06-17 01:08:41 +0100

    finilazing admin dashboard and functionalities

--- src/Form/CandidatType.php
+++ src/Form/CandidatType.php
@@ -2,31 +2,52 @@
 
 namespace App\Form;
 
-use App\Entity\Candidat\Candidat;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CandidatType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('Age')
-            ->add('Email')
-            ->add('categorie',EntityType::class,[
-                'class'=>'App\Entity\Categorie\Categorie'
-            ])
-        ;
+        ->add('username')
+        ->add('email')
+        ->add('Nom')
+        ->add('Prenom')
+        ->add('Age')
+        ->add('Adresse')
+        ->add('Telephone')
+        ->add('plainPassword', PasswordType::class, [
+            // instead of being set onto the object directly,
+            // this is read and encoded in the controller
+            'mapped' => false,
+            'attr' => ['autocomplete' => 'new-password'],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Please enter a password',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    // max length allowed by Symfony for security reasons
+                    'max' => 4096,
+                ]),
+            ],
+        ])
+    ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Candidat::class,
+            'data_class' => User::class,
         ]);
     }
 }
